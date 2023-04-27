9362045c43bbd6e799911d52e91010b00855ffce
andishe <a.abbasian@pixaldo.com>
2023-04-27 13:41:29 +0430

    updated configes and components

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Layout from "../layout";
 
-const Card = () => {
+function Card(props) {
   return (
-    <div>Card</div>
-  )
+    <Layout>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src={props.image} alt={props.alt} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{props.title}</div>
+          <p className="text-gray-700 text-base">{props.description}</p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {props.tag1}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+            {props.tag2}
+          </span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default Card
+export default Card;
---
+++ src/layout/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../../index.css";
+const Layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
